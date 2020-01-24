0f852bc542592e24e19d318f85a51483e26e5318
Magdalena <42957812+umagda@users.noreply.github.com>
2020-01-24 22:02:43 +0100

    Stworzone pakiety dla różnych sfer odpowiedzialności

---
+++ test/test_add_group3.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import pytest
+from model.group import Group
+from fixture.application import Application
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+def test_add_group(app):
+    app.open_home_page()
+    app.login(username="admin", password="secret")
+    app.open_groups_page()
+    app.create_group(Group(name="abc", header="123", footer="def"))
+    app.return_to_groups_page()
+    app.logout()
+
+def test_add_empty_group(app):
+    app.open_home_page()
+    app.login(username="admin", password="secret")
+    app.open_groups_page()
+    app.create_group(Group(name="", header="", footer=""))
+    app.return_to_groups_page()
+    app.logout()
+
