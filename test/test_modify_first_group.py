383528cfb0e7f2b38ca93c56bc3b09f39336c72c
Magdalena <42957812+umagda@users.noreply.github.com>
2020-02-03 16:04:01 +0100

    Homework no. 7

---
+++ test/test_modify_first_group.py
@@ -0,0 +1,6 @@
+from model.group import Group
+
+def test_modify_first_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(name="nowa grupa", header="abc", footer="123"))
+    app.session.logout()
